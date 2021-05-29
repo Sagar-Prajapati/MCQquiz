f756eced25e82ca22abe70bf595e282ecd60d98c
Sagar-Prajapati <69591215+Sagar-Prajapati@users.noreply.github.com>
2021-05-29 09:34:50 +0530

    Update server.js
    knhooohno;n

--- server.js
+++ server.js
@@ -4,32 +4,33 @@ const mongoose = require('mongoose');
 const AppError = require('./util/appErrors');
 const facultyRoute = require('./routes/faculty.route');
 const studentRoute = require('./routes/student.route');
-const {globalErrorController} = require('./util/errors');
+const { globalErrorController } = require('./util/errors');
 
 require('dotenv').config();
 
-
-const app=express();
-
-
+const app = express();
 
 app.use(express.json());
-app.use(cors({exposedHeaders:"x-auth-token"}));
-app.use('/api/faculty',facultyRoute);
-app.use('/api/student',studentRoute);
-app.all("*", (req, res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-  });
+app.use(cors({ exposedHeaders: 'x-auth-token' }));
+app.use('/api/faculty', facultyRoute);
+app.use('/api/student', studentRoute);
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 app.use(globalErrorController);
-  
 
+console.log('a');
 
-
-mongoose.connect(process.env.DATABASE_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useFindAndModify:false
-}).then(()=>{
+mongoose
+  .connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => {
     app.listen(process.env.PORT);
-    console.log('connected at port '+process.env.PORT);
-}).catch(err=>{console.log(err)});
+    console.log('connected at port ' + process.env.PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
